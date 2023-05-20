4188b8b97f4bee074da38108f3ecb59531562b81
qurriahSam <mugirethe1@gmail.com>
2023-05-20 21:16:13 +0300

    tests: check if objects and their children are equal

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -45,10 +45,10 @@ suite("Unit Tests", function () {
       assert.strictEqual(6 * "2", 12);
       assert.notStrictEqual([1, "a", {}], [1, "a", {}]);
     });
-    // #7
+    // #7 check if objects and their children are equal using ==
     test("#deepEqual, #notDeepEqual", function () {
-      assert.fail({ a: "1", b: 5 }, { b: 5, a: "1" }, "The order of keys doesn't matter");
-      assert.fail({ a: [5, 6] }, { a: [6, 5] }, "The order of array elements does matter");
+      assert.deepEqual({ a: "1", b: 5 }, { b: 5, a: "1" }, "The order of keys doesn't matter");
+      assert.notDeepEqual({ a: [5, 6] }, { a: [6, 5] }, "The order of array elements does matter");
     });
   });
 
